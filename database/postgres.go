5d7f5509d40a408c84d9d9ae69b640adf55ce823
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-05-11 16:01:58 +0200

    expand functionality and fix database issues

--- database/postgres.go
+++ database/postgres.go
@@ -34,13 +34,14 @@ func (p *Postgres) Insert(table string, columns []string, val []interface{}) err
 
 	for i := range columns {
 		if valuesPlaceholder == "" {
-			valuesPlaceholder = fmt.Sprintf("$%d", i)
+			valuesPlaceholder = fmt.Sprintf("$%d", i + 1)
 		} else {
-			valuesPlaceholder = fmt.Sprintf(", $%d", i)
+			valuesPlaceholder += fmt.Sprintf(", $%d", i + 1)
 		}
 	}
 
 	query := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", table, strings.Join(columns, ", "), valuesPlaceholder)
+	klog.Infof("database query insert: %s\n", query)
 	_, err := p.db.Exec(query, val...)
 
 	return err
@@ -59,19 +60,20 @@ func (p *Postgres) Query(table string, columns []string, parameters []string, va
 
 	for i, v := range parameters {
 		if parameter == "" {
-			parameter = fmt.Sprintf("%v = $%d", v, i)
+			parameter = fmt.Sprintf("%v = $%d", v, i + 1)
 		} else {
-			parameter += fmt.Sprintf("AND %v = $%d", v, i)
+			parameter += fmt.Sprintf("AND %v = $%d", v, i + 1)
 		}
 	}
 
 	var query string
 	if len(parameters) == 0 {
-		query = fmt.Sprintf("SELECT %s FROM %s")
+		query = fmt.Sprintf("SELECT %s FROM %s", strings.Join(columns, ", "), table)
 	} else {
-		query = fmt.Sprintf("SELECT %s FROM %s WHERE %s", strings.Join(columns, ", "), table, parameters)
+		query = fmt.Sprintf("SELECT %s FROM %s WHERE %s", strings.Join(columns, ", "), table, parameter)
 	}
 
+	klog.Infof("Database query: %s", query)
 	quResult, err := p.db.Query(query, parameterValue...)
 	if err != nil {
 		return err
@@ -125,6 +127,30 @@ func (p *Postgres) Query(table string, columns []string, parameters []string, va
 		valueCounter++
 	}
 
+	return err
+}
+
+func (p *Postgres) Delete(table string, paramters []string, paramValues []interface{}) error{
+	var clause, query string
+	if len(paramters) != len(paramValues) {
+		return fmt.Errorf("len of paramters is not equal to len of paramValues")
+	}
+
+	for i, v := range paramters {
+		if clause == "" {
+			clause = fmt.Sprintf("%s = $%d", v, i + 1)
+		} else {
+			clause += fmt.Sprintf("AND %s = $%d", v, i + 1)
+		}
+	}
+
+	if clause == "" {
+		query = fmt.Sprintf("DELETE FROM %s", table)
+	} else {
+		query = fmt.Sprintf("DELETE FROM %s WHERE %s", table, clause)
+	}
+
+	_, err := p.db.Exec(query, paramValues...)
 	return err
 }
 
