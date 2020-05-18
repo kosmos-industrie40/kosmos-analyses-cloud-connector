3354ef126025844f7dec331831f150d4053079b9
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-05-18 20:09:17 +0200

    layout models/contract

--- models/contract.go
+++ models/contract.go
@@ -1,18 +1,18 @@
 package models
 
 type Contract struct {
-	ModelsCloud []Model `json:"modelsCloud"`
-	ModelsEdge []Model `json:"modelsEdge"`
-	ContractId string `json:"contractId"`
-	Machines []ContractMachines `json:"machines"`
+	ModelsCloud []Model            `json:"modelsCloud"`
+	ModelsEdge  []Model            `json:"modelsEdge"`
+	ContractId  string             `json:"contractId"`
+	Machines    []ContractMachines `json:"machines"`
 }
 
 type ContractMachines struct {
-	MachineId string `json:"machineId"`
-	Sensors []ContractSensors `json:"sensors"`
+	MachineId string            `json:"machineId"`
+	Sensors   []ContractSensors `json:"sensors"`
 }
 
 type ContractSensors struct {
-	SensorId string `json:"sensorId"`
-	Model []Model `json:"model"`
+	SensorId string  `json:"sensorId"`
+	Model    []Model `json:"model"`
 }
