92e8e5029ee85d59d21575af889943d489d39ee1
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-05-29 11:09:13 +0200

    fix functionality of endpoint analyses

--- endpoints/analyses.go
+++ endpoints/analyses.go
@@ -10,6 +10,7 @@ import (
 
 	"gitlab.inovex.de/proj-kosmos/kosmos-analyses-cloud-connector/database"
 	"gitlab.inovex.de/proj-kosmos/kosmos-analyses-cloud-connector/logic"
+	"gitlab.inovex.de/proj-kosmos/kosmos-analyses-cloud-connector/models"
 )
 
 type Analyses struct {
@@ -47,7 +48,7 @@ func (a Analyses) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			klog.Errorf("unexpected count of parameters in query: %d\n", len(ur))
 			w.WriteHeader(400)
 			return
-		case 2:
+		case 3:
 			parsedQuery := make(map[string][]string)
 			contractId := ur[2]
 			queryParam := r.URL.Query()
@@ -68,6 +69,11 @@ func (a Analyses) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
+			if string(retValue) == "null" {
+				w.WriteHeader(200)
+				return
+			}
+
 			if _, err := w.Write(retValue); err != nil {
 				klog.Errorf("could not write result: %s\n", err)
 				w.WriteHeader(500)
@@ -75,7 +81,7 @@ func (a Analyses) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			}
 			return
 
-		case 3:
+		case 4:
 			if ur[3] == "" {
 				// same as len(ur) == 2
 				parsedQuery := make(map[string][]string)
@@ -98,6 +104,11 @@ func (a Analyses) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 					return
 				}
 
+				if string(retValue) == "null" {
+					w.WriteHeader(200)
+					return
+				}
+
 				if _, err := w.Write(retValue); err != nil {
 					klog.Errorf("could not write result: %s\n", err)
 					w.WriteHeader(500)
@@ -108,6 +119,7 @@ func (a Analyses) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				ret, err := logic.GetSpecificResult(ur[2], ur[3], a.Db)
 				if err != nil {
 					klog.Errorf("could not query specific result: %s\n", err)
+					w.WriteHeader(500)
 				}
 				if _, err := w.Write(ret); err != nil {
 					klog.Errorf("could send result: %s\n", err)
@@ -117,17 +129,32 @@ func (a Analyses) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	case "POST":
 		ur := strings.Split(r.URL.Path, "/")
+		if len(ur) != 5 {
+			klog.Errorf("unexpected length of url path: %d\n", len(ur))
+			w.WriteHeader(400)
+			return
+		}
+
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			klog.Errorf("could not read data from request body; err : %v\n", err)
 			w.WriteHeader(400)
 			return
 		}
-		if err := logic.InsertResult(ur[2], ur[3], ur[4], body, a.Db); err != nil {
-			klog.Errorf("could not insert data: %s\n", err)
-			w.WriteHeader(500)
+
+		var data []models.UploadResult
+		if err := json.Unmarshal(body, &data); err != nil {
+			klog.Errorf("could not parse data: %s\n", err)
+			w.WriteHeader(400)
 			return
 		}
+
+		if err := logic.InsertResult(ur[2], ur[3], ur[4], data, a.Db); err != nil {
+			w.WriteHeader(500)
+			klog.Errorf("could not insert data: %s\n", err)
+			return
+		}
+
 		w.WriteHeader(201)
 		return
 	}
