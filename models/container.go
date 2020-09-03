bd87147d65ad16118193b3c568a0e4cd3e0d448a
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-09-03 14:04:38 +0200

    adding container model

---
+++ models/container.go
@@ -0,0 +1,83 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	"k8s.io/klog"
+)
+
+type Container struct {
+	Url         string   `json:"url"`
+	Tag         string   `json:"tag"`
+	Arguments   []string `json:"arguments"`
+	Environment []string `json:"environment"`
+}
+
+func (c Container) Exists(db *sql.DB) (bool, int64, error) {
+	result, err := db.Query("SELECT id FROM containers WHERE url = $1 AND tag = $2 AND arguments = $3 AND environment = $4", c.Url, c.Tag, c.Arguments, c.Environment)
+	if err != nil {
+		return false, 0, err
+	}
+
+	defer func() {
+		if err := result.Close(); err != nil {
+			klog.Errorf("could not close row type: %s\n", err)
+		}
+	}()
+
+	var id int64
+
+	if !result.Next() {
+		return false, 0, nil
+	}
+
+	result.Scan(&id)
+
+	return true, id, nil
+}
+
+func (c Container) Insert(db *sql.DB) (int64, error) {
+	result, err := db.Exec("INSERT INTO containers (url, tag, arguments, environment) VALUES ($1, $2, $3, $4) RETURNING id", c.Url, c.Tag, c.Arguments, c.Environment)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (c Container) Query(db *sql.DB, id int64) error {
+	result, err := db.Query("SELECT url, tag, arguments, environment FROM containers WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := result.Close(); err != nil {
+			klog.Errorf("could not close result: %v\n", err)
+		}
+	}()
+
+	var url, tag string
+	var arguments, environment []string
+
+	if !result.Next() {
+		return fmt.Errorf("no container found to id: %d\n", id)
+	}
+
+	if err := result.Scan(&url, &tag, &arguments, &environment); err != nil {
+		return err
+	}
+
+	c.Url = url
+	c.Tag = tag
+	c.Arguments = arguments
+	c.Environment = environment
+
+	return nil
+}
