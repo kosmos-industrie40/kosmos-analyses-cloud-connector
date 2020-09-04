3afdfd379d9a4bef7b7fdfd201bc85c2f854b6ec
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-09-04 12:42:27 +0200

    adding machine model

--- models/machine.go
+++ models/machine.go
@@ -1,34 +1,170 @@
 package models
 
-type Data struct {
-	Machine   string        `json:"machine,omitempty"`
-	Sensor    string        `json:"sensor"`
-	MessageId int           `json:"message_id,omitempty"`
-	From      string        `json:"from"`
-	Columns   []DataColumnn `json:"columns"`
-	Data      [][]string    `json:"data"`
-	Meta      []DataMeta    `json:"meta"`
-	Signature string        `json:"signature,omitempty"`
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"k8s.io/klog"
+)
+
+type Machine struct {
+	ID      string
+	Meta    interface{}
+	Sensors []Sensor
 }
 
-type DataColumnn struct {
-	Name          string `json:"name"`
-	Type          string `json:"type"`
-	AllowedValues string `json:"allowed_values,omitempty"`
+func (m *Machine) Exists(db *sql.DB) (bool, []int64, error) {
+	metaB, err := json.Marshal(m.Meta)
+	if err != nil {
+		return false, nil, err
+	}
+
+	meta := string(metaB)
+
+	result, err := db.Query("SELECT exists(SELECT true FROM machines WHERE id = $1 AND meta = $2)", m.ID, meta)
+	if err != nil {
+		return false, nil, err
+	}
+
+	defer func() {
+		if err := result.Close(); err != nil {
+			klog.Errorf("cannot close query object: %s\n", err)
+		}
+	}()
+
+	var exists bool
+	if !result.Next() {
+		return false, nil, fmt.Errorf("can not check existens in query")
+	}
+
+	if err := result.Scan(&exists); err != nil {
+		return false, nil, nil
+	}
+
+	if !exists {
+		// no machines found
+		return false, nil, nil
+	}
+
+	ids, err := db.Query("SELECT sensor FROM machine_sensors WHERE machine = $1", m.ID)
+	if err != nil {
+		return true, nil, err
+	}
+
+	defer func() {
+		if err := ids.Close(); err != nil {
+			klog.Errorf("cannot close query object: %s\n", err)
+		}
+	}()
+
+	var machineSensorIds []int64
+
+	for !ids.Next() {
+		var id int64
+		if err := ids.Scan(&id); err != nil {
+			return true, nil, err
+		}
+
+		machineSensorIds = append(machineSensorIds, id)
+	}
+
+	return true, machineSensorIds, nil
 }
 
-type DataMeta struct {
-	Name        string `json:"name"`
-	Type        string `json:"type"`
-	Description string `json:"description,omitempty"`
-	Value       string `json:"value,omitempty"`
+func (m *Machine) Query(db *sql.DB, id string) error {
+	result, err := db.Query("SELECT meta FROM machines WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := result.Close(); err != nil {
+			klog.Errorf("cannot close query object: %s\n", err)
+		}
+	}()
+
+	var meta string
+	if !result.Next() {
+		return fmt.Errorf("no matching machine found")
+	}
+
+	if err := result.Scan(&meta); err != nil {
+		return err
+	}
+
+	var metaDe interface{}
+	if err := json.Unmarshal([]byte(meta), &metaDe); err != nil {
+		return err
+	}
+
+	m.Meta = metaDe
+	m.ID = id
+
+	querySensors, err := db.Query("SELECT sensor FROM machine_sensors WHERE id = $1 GROUP BY sensor", id)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := querySensors.Close(); err != nil {
+			klog.Errorf("cannot close query object: %s\n", err)
+		}
+	}()
+
+	var sensorIds []int64
+
+	for !querySensors.Next() {
+		var id int64
+		if err := querySensors.Scan(&id); err != nil {
+			return err
+		}
+
+		sensorIds = append(sensorIds, id)
+	}
+
+	for _, v := range sensorIds {
+		sensor := Sensor{}
+		if err := sensor.Query(db, v); err != nil {
+			return err
+		}
+		m.Sensors = append(m.Sensors, sensor)
+	}
+
+	return nil
 }
 
-type SendData struct {
-	Machine string        `json:"machine,omitempty"`
-	Sensor  string        `json:"string,omitempty"`
-	From    string        `json:"from"`
-	Columns []DataColumnn `json:"columns"`
-	Data    [][]string    `json:"data"`
-	Meta    []DataMeta    `json:"meta"`
+func (m *Machine) Insert(db *sql.DB) ([]int64, error) {
+	metaB, err := json.Marshal(m.Meta)
+	if err != nil {
+		return nil, err
+	}
+
+	meta := string(metaB)
+
+	if _, err := db.Exec("INSERT INTO machines (id, meta) VALUES ($1, $2)", m.ID, meta); err != nil {
+		return nil, err
+	}
+
+	var msIds []int64
+	for _, sensor := range m.Sensors {
+		id, err := sensor.Insert(db)
+		if err != nil {
+			return nil, err
+		}
+
+		ms, err := db.Exec("INSERT INTO machine_sensors (machine, sensor) VALUES ($1, $2) RETURNING id", m.ID, id)
+		if err != nil {
+			return nil, err
+		}
+
+		msId, err := ms.LastInsertId()
+		if err != nil {
+			return nil, err
+		}
+
+		msIds = append(msIds, msId)
+	}
+
+	return msIds, nil
 }
