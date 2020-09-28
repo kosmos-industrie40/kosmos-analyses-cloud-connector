52b92e01c0a27e8384aa470ee590ab54950bc9c4
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-09-28 13:10:07 +0200

    implement auth helper

---
+++ src/endpoints/auth/helper.go
@@ -0,0 +1,162 @@
+package auth
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"k8s.io/klog"
+)
+
+// AuthHelper is an helper interface, which can check if an user is authenticated or not
+type Helper interface {
+	// IsAuthenticated consumes the token and checked if this token is valid
+	// returns an error on error or true if the authentication is valid
+	// in other cases (no error and not authenticated) you can use the status code
+	IsAuthenticated(*http.Request, string, bool) (bool, int, error)
+
+	// CreateSession will create a session on a specific token. This token will be used
+	// to identify if the user, has the required permission or not
+	CreateSession(string, []string, time.Time) error
+
+	// DeleteSession will delete a user session, which is identified by a the session token
+	DeleteSession(string) error
+
+	// CleanUp will run every hour and will remove all invalid tokens
+	CleanUp()
+}
+
+type helperOidc struct {
+	db *sql.DB
+}
+
+func (a helperOidc) cleanUp() error {
+	res, err := a.db.Exec("DELETE FROM token WHERE valid < NOW()")
+	if err != nil {
+		return fmt.Errorf("cannot delete invalid tokens: %s", err)
+	}
+
+	columns, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("cannot get the number of rows: %s", err)
+	}
+	klog.Infof("removing %d invalid tokens", columns)
+	return nil
+}
+
+func (a helperOidc) CleanUp() {
+	for {
+		time.Sleep(time.Hour)
+		if err := a.cleanUp(); err != nil {
+			klog.Error(err)
+		}
+	}
+}
+
+func (a helperOidc) CreateSession(token string, organisations []string, valid time.Time) error {
+	query, err := a.db.Query("SELECT id FROM organisations WHERE name in ($1)", strings.Join(organisations, ","))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := query.Close(); err != nil {
+			klog.Errorf("cannot close query object: %s", err)
+		}
+	}()
+
+	var orgs []int64
+	for query.Next() {
+		var organisation int64
+		if err := query.Scan(&organisation); err != nil {
+			return err
+		}
+
+		orgs = append(orgs, organisation)
+	}
+
+	if len(orgs) == 0 {
+		return fmt.Errorf("no matching organisations found")
+	}
+
+	if _, err := a.db.Exec("INSERT INTO token (token, valid) VALUES ($1, $2)", token, valid); err != nil {
+		return err
+	}
+
+	for _, org := range orgs {
+		klog.Infof("insert token_permission with (%s, %d)", token, org)
+		if _, err := a.db.Exec("INSERT INTO token_permission (token, organisation) VALUES ($1, $2)", token, org); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a helperOidc) IsAuthenticated(request *http.Request, contract string, write bool) (bool, int, error) {
+	token := request.Header.Get("token")
+	if token == "" {
+		klog.Infof("no token can be found")
+		return false, http.StatusUnauthorized, nil
+	}
+
+	query, err := a.db.Query("SELECT valid FROM token WHERE token = $1", token)
+	if err != nil {
+		return false, http.StatusInternalServerError, err
+	}
+
+	defer func() {
+		if err := query.Close(); err != nil {
+			klog.Errorf("cannot close query object: %s", err)
+		}
+	}()
+
+	if !query.Next() {
+		return false, http.StatusUnauthorized, nil
+	}
+
+	var valid time.Time
+	if err := query.Scan(&valid); err != nil {
+		klog.Infof("cannot scan valid time")
+		return false, http.StatusInternalServerError, err
+	}
+
+	if time.Now().After(valid) {
+		klog.Infof("timestamp.after doesn't match")
+		return false, http.StatusUnauthorized, nil
+	}
+
+	var table string
+	if write {
+		table = "write_permissions rp"
+	} else {
+		table = "read_permissions rp"
+	}
+	hasPermission, err := a.db.Query(fmt.Sprintf("SELECT tp.organisation FROM token_permission as tp JOIN %s on tp.organisation = rp.organisation WHERE token = $1 AND contract = $2", table), token, contract)
+	if err != nil {
+		return false, http.StatusInternalServerError, err
+	}
+
+	defer func() {
+		if err := hasPermission.Close(); err != nil {
+			klog.Errorf("cannot close query object: %s", err)
+		}
+	}()
+
+	if !hasPermission.Next() {
+		return false, http.StatusUnauthorized, nil
+	}
+
+	return true, 0, nil
+}
+
+func (a helperOidc) DeleteSession(token string) error {
+	_, err := a.db.Exec("DELETE FROM token WHERE token = $1", token)
+	return err
+}
+
+// NewAuthHelper creates a new authentication auth helper
+func NewAuthHelper(db *sql.DB) Helper {
+	return helperOidc{db: db}
+}
